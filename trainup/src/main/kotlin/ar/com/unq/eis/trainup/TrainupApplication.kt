b5fc077a4375b4daff0f1f7bfe9dfc707fbbe974
Elian Borda <eliancamiloalejandro@gmail.com>
2024-09-06 12:52:56 -0300

    init

---
+++ trainup/src/main/kotlin/ar/com/unq/eis/trainup/TrainupApplication.kt
@@ -0,0 +1,11 @@
+package ar.com.unq.eis.trainup
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TrainupApplication
+
+fun main(args: Array<String>) {
+	runApplication<TrainupApplication>(*args)
+}
---
+++ trainup/src/test/kotlin/ar/com/unq/eis/trainup/TrainupApplicationTests.kt
@@ -0,0 +1,13 @@
+package ar.com.unq.eis.trainup
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TrainupApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
