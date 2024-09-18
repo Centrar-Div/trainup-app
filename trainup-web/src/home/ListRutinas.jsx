cb0ca89ed29e0f9a36335c934209e53e3580bbe9
Agusdiisanto <agusdisanto99@gmail.com>
2024-09-18 13:09:10 -0300

    Refresh

--- trainup-web/src/home/ListRutinas.jsx
+++ trainup-web/src/home/ListRutinas.jsx
@@ -5,7 +5,6 @@ import { useNavigate } from 'react-router-dom';
 import { useLogin } from '../context/LoginContext';
 import { completarRutina } from '../api/Api';
 import NotRutins from '../utils/NotRutins';
-import Loader from '../utils/Loader';
 
 const ListRutinas = ({ rutinas, esCompletada }) => {
   const navigate = useNavigate();
@@ -28,7 +27,6 @@ const ListRutinas = ({ rutinas, esCompletada }) => {
   };
 
   const marcarComoCompletada = async () => {
-    setLoading(true); 
     try {
       await completarRutina(user.id, rutinaIdToComplete);
       notification.success({
@@ -36,21 +34,19 @@ const ListRutinas = ({ rutinas, esCompletada }) => {
         description: 'Se ha completado la rutina correctamente.',
         placement: 'topRight',
       });
-      setIsModalVisible(false);
+      setIsModalVisible(false); 
+      window.location.reload();
     } catch (error) {
       notification.error({
         message: 'Error',
         description: 'Hubo un problema al completar la rutina. Inténtalo de nuevo.',
         placement: 'topRight',
       });
-    } finally {
-      setLoading(false);
     }
   };
 
   return (
     <div className="container-boxinfo">
-      {loading && <Loader />}
       {
         rutinas && rutinas.length > 0 ? (
           rutinas.map(rutina => (
