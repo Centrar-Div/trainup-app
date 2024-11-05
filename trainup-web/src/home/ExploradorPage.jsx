4906b46fd1a32e67fd7cbc0b3d6b938fa7daa4bd
Agustin Di Santo <agusdisanto99@gmail.com>
2024-11-05 20:25:43 -0300

    Fixed Loader

--- trainup-web/src/home/ExploradorPage.jsx
+++ trainup-web/src/home/ExploradorPage.jsx
@@ -1,28 +1,29 @@
-import React, { useEffect, useState } from 'react'
-import { obtenerRutinas } from '../api/Api'
-import CardRutinaSimple from './CardRutinaSimple'
-import NotRutins from '../utils/NotRutins'
+import React from 'react';
+import { obtenerRutinas } from '../api/Api';
+import CardRutinaSimple from './CardRutinaSimple';
+import NotRutins from '../utils/NotRutins';
+import DataLoader from '../utils/DataLoader';
 
 const ExploradorPage = () => {
-
-    const [rutinas, setRutinas] = useState([])
-
-    useEffect(() => {
-        obtenerRutinas().then(({data}) =>  setRutinas(data))
-    }, [rutinas])
-
   return (
-    <div className='temp-content-body '>
-        { rutinas.length > 0 ?
-            rutinas.map((rutina) => <CardRutinaSimple rutina={rutina} />):
-            <NotRutins 
-                titulo="No existen rutinas todavia" 
-                mensaje="Nadie ha creado rutinas todavia, se el primero en crear una"
-                showButton={false}
+    <div className='temp-content-body'>
+      <DataLoader fetchData={obtenerRutinas}>
+        {(rutinas) =>
+          rutinas.length > 0 ? (
+            rutinas.map((rutina, index) => (
+              <CardRutinaSimple key={index} rutina={rutina} />
+            ))
+          ) : (
+            <NotRutins
+              titulo="No existen rutinas todavia"
+              mensaje="Nadie ha creado rutinas todavia, se el primero en crear una"
+              showButton={false}
             />
+          )
         }
+      </DataLoader>
     </div>
-  )
-}
+  );
+};
 
-export default ExploradorPage
+export default ExploradorPage;
